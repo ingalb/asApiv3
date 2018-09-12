90cef6c0b6e8ae7e23f22b3ef7235a2ecd74f391
ingalb <ingalbi@gmail.com>
2018-09-12 17:28:47 +0200

    Update ekipi.php

--- ekipi.php
+++ ekipi.php
@@ -16,16 +16,16 @@ case "kombetarja":
   $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/".$id."/4.html?format=raw&type=".$type."&app_id=".$app_id;
   break;
 case "superliga":
-  $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/117/13.html?format=raw&type=".$type."&app_id=".$app_id;
+  $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/121/13.html?format=raw&type=".$type."&app_id=".$app_id;
   break;
 case "kategoria1":
-    $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/98/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
+    $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/121/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
     break;
 case "kosova":
     $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/96/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
     break;
 case "kupa":
-    $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/95/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
+    $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/123/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
     break;
 default:
   $url="http://www.albaniasoccer.com/statistika/shqiperi/kombetarja/roster/".$id."/".$ekipi.".html?format=raw&type=".$type."&app_id=".$app_id;
